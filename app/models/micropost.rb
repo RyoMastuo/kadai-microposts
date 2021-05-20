c8957f2c02c20814b8b69f116a56ae78fb370fad
ryo.matsuo <ryo072024@gmail.com>
2021-05-20 08:42:22 +0000

    revision

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
-  has_many :likeds, through: :reverses_of_favorite, source: :user
+  has_many :favorites, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :likeds, through: :favorites, source: :user
   
   validates :content, presence: true, length: { maximum: 255 }
 end
